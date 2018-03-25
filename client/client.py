5d1c93bfc7bd5e3827b0300e6e847651998a763b
lpineda <leandropineda.lp@gmail.com>
2018-03-25 14:06:44 -0300

    Added multithreading support for msg publisher

--- client/client.py
+++ client/client.py
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
+
 import time
 import logging
 import paho.mqtt.client as mqtt
 import random
 import argparse
+from multiprocessing.dummy import Pool as ThreadPool 
+
+def configure_logger(logger):
+        logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(threadName)s.%(name)s.%(funcName)s.%(lineno)d - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
 
 class MqttErrors(object):
     def __init__(self):
@@ -37,13 +48,8 @@ class MqttErrors(object):
 class MqttClient(object):
     def __init__(self, server_address):
 
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.DEBUG)
-        ch = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(funcName)s.%(lineno)d - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-
-        self.logger.addHandler(ch)
+        self.logger = logging.getLogger(self.__class__.__name__)
+        configure_logger(self.logger)
 
         self.logger.info("Initializing " + __name__)
 
@@ -77,12 +83,10 @@ class MqttClient(object):
             time.sleep(1)
         try:
             mqtt_msg_info = self.client.publish(self.topic, event)
-            self.client.loop(timeout=3)
             return_code, msg_id = mqtt_msg_info
+            mqtt_msg_info.wait_for_publish()
             #self.logger.debug("Message ID {}. Return code {}.".format(msg_id, return_code))
-            if return_code == mqtt.MQTT_ERR_NO_CONN:
-                self.logger.error("Client not connected.")
-            if not mqtt_msg_info.is_published():
+            if return_code:
                 self.logger.error("Error publishing message. Return code {}: {}".format(return_code, self.error_description.get(return_code)))
 
         except Exception as ex:
@@ -96,53 +100,82 @@ class MqttClient(object):
         self.logger.warn("Connection lost. Result code {}: {}".format(rc, self.error_description.get(rc)))
         self._is_connected = False
 
-messages = dict()
-messages['Message1'] = 50
-messages['Message2'] = 100
-messages['Message3'] = 4
-messages['Message4'] = 20
-messages['Message5'] = 4
-messages['Message6'] = 5
-messages['Message7'] = 4
-messages['Message8'] = 10
-messages['Message9'] = 1
-messages['Message10'] = 1
+class MqttPublisher(object):
+    def __init__(self, server_address):
 
-messages_universe = list()
-for k, v in messages.iteritems():
-    for _ in range(v):
-        messages_universe.append(k)
+        self.logger = logging.getLogger(self.__class__.__name__)
+        configure_logger(self.logger)
 
-random.shuffle(messages_universe)
+        self.server_address = server_address
+        messages = dict()
+        messages['Message1'] = 50
+        messages['Message2'] = 100
+        messages['Message3'] = 4
+        messages['Message4'] = 20
+        messages['Message5'] = 4
+        messages['Message6'] = 5
+        messages['Message7'] = 4
+        messages['Message8'] = 10
+        messages['Message9'] = 1
+        messages['Message10'] = 1
+
+        self.messages_universe = list()
+        for k, v in messages.iteritems():
+            for _ in range(v):
+                self.messages_universe.append(k)
+
+        random.shuffle(self.messages_universe)
+        self.len_messages_universe = len(self.messages_universe)
+
+        self.client = MqttClient(self.server_address)
+
+    def publish_random_event(self):
+        n = random.randint(0, self.len_messages_universe - 1)
+        try:
+            self.client.publish(self.messages_universe[n])
+        except Exception as ex:
+            print ex.message
+
+    def publish(self, n_events):
+        if n_events == 0:
+            self.logger.info("Publishing events.")
+            while True:
+                self.publish_random_event()
+        else:
+            self.logger.info("Publishing {} events.".format(n_events))
+            for e in range(n_events):
+                self.publish_random_event()
+
+class MqttPublisherThreading(object):
+    def __init__(self, server_address, n_threads, n_events):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        configure_logger(self.logger)
+
+        self.server_address = server_address
+        self.n_threads = int(n_threads)
+        self.n_events = n_events
+        self.pool = ThreadPool(self.n_threads)
+        self.logger.info("Threads {}. Events {}.".format(n_threads, n_events))
+
+    def publish(self):
+        def build_worker(n_events, obj = MqttPublisher(self.server_address)):
+            self.logger.info("Building worker")
+            obj.publish(n_events)
+        results = self.pool.map(build_worker, [self.n_events for _ in range(self.n_threads)])
+
+
+
 
 parser = argparse.ArgumentParser(description='Generate events and publish them to mqtt topic.')
-parser.add_argument('ip', metavar='N', type=str, help='IP Address')
+parser.add_argument('server_address', metavar='N', type=str, help='IP Address')
 parser.add_argument('--n_events', metavar='N', type=str,  help='How many events will be generated')
+parser.add_argument('--n_threads', metavar='N', type=str,  help='How many threads will be used')
 
 args = parser.parse_args()
-ip = args.ip
-n_events = args.n_events
+server_address = args.server_address
+n_events = 0 if args.n_events is None else int(args.n_events)
+n_threads = 1 if args.n_threads is None else int(args.n_threads)
 
-client = MqttClient(ip)
-
-if not n_events:
-    print("Publishing infinite events.")
-    while True:
-        n = random.randint(0, len(messages_universe)-1)
-        try:
-            client.publish(messages_universe[n])
-        except Exception as ex:
-            print "ERROR"
-            print type(ex)
-else:
-    n_events = int(n_events)
-    print("Publishing {} events.".format(n_events))
-
-    for _ in range(n_events):
-        n = random.randint(0, len(messages_universe)-1)
-        try:
-            client.publish(messages_universe[n])
-        except Exception as ex:
-            print "ERROR"
-            print type(ex)
+mqtt_publisher = MqttPublisherThreading(server_address, n_threads, n_events)
+mqtt_publisher.publish()
 
